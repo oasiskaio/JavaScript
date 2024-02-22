30e05e460f3c44f06e8614972076e11c6ec748fa
Kaio xavier <152352968+oasiskaio@users.noreply.github.com>
2024-02-22 12:52:22 -0300

    alterações
    s

--- 09 - Node, Npm, Express e MongoDB/Aula 12 - Expres+Webpack/server.js
+++ 09 - Node, Npm, Express e MongoDB/Aula 12 - Expres+Webpack/server.js
@@ -2,7 +2,7 @@
 // npm i ejs
 const express = require('express');
 const app = express();
-const routes = require('./routes')
+const routes = require('./routes');
 const path = require('path');
 
 app.use( express.urlencoded( { extended: true } ));
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 // usa tudo que foi declarado nas routes.js
 app.use(routes)
 
-app.listen(4000, () => {
+app.listen(3000, () => {
     console.log('Acessar http://localhost:3000')
     console.log('Servidor executando na porta 3000')
 })
