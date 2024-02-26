7e71bcea0e69e938b7fecb7ac82cfb954e5314dd
Kaio xavier <152352968+oasiskaio@users.noreply.github.com>
2024-02-26 08:40:41 -0300

    coment

--- 09 - Node/Aula 14 - Conexão MongoDB/src/models/HomeModel.js
+++ 09 - Node/Aula 14 - Conexão MongoDB/src/models/HomeModel.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 
+// novo esquema mongoose
+// com a configuração dos dados que queremos
+// moongose nao ta nem ai como mandamos os dados, cabe a nos tratar
 const HomeSchema = new mongoose.Schema({
     titulo: { type: String, require: true},
     descricao: String
