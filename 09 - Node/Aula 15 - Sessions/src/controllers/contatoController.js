9f5b66097dd308881eef8182ca247b0028f9e689
Kaio xavier <152352968+oasiskaio@users.noreply.github.com>
2024-02-28 11:52:40 -0300

    fatoração

--- 09 - Node/Aula 15 - Sessions/src/controllers/contatoController.js
+++
@@ -1,6 +0,0 @@
-// essas funções são exportadas para serem usadas nas rotas quando necessario
-
-// função do controlador de contato que trata do caminho de contatos
-exports.paginaInicial = (req, res) => {
-   res.send('Obrigado por entrar em contato.');
-}
--- 09 - Node/Aula 15 - Sessions/src/middlewares/middleware.js
+++
@@ -1,6 +0,0 @@
-exports.middlewareGlobal = (req, res, next) => {
-    if(req.body.cliente) {
-    console.log(`O que voce postou foi: ${req.body.cliente}`);
-    };
-    next()
-};
